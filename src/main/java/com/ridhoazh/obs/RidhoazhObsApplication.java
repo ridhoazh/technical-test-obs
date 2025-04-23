c7e90644323370b32f10cd4f03586877fd1a6fe0
ridhoazh <ridhoazhari.core@gmail.com>
2025-04-23 21:45:43 +0700

    Initiate project for recruitment test OBS

---
+++ src/main/java/com/ridhoazh/obs/RidhoazhObsApplication.java
@@ -0,0 +1,13 @@
+package com.ridhoazh.obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidhoazhObsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidhoazhObsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridhoazh/obs/RidhoazhObsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridhoazh.obs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidhoazhObsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
